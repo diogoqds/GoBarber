e47daca39a5eed9f269b4607bd54d2ec4a7bc4a1
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-04-26 17:12:59 -0300

    create middlewares to redirect user

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,20 @@ const multer = require('multer')
 const routes = express.Router()
 const multerConfig = require('./config/multer')
 const upload = multer(multerConfig)
+
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/', SessionController.create)
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
 routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
   return res.send('logado')
 })
 
